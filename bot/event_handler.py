fbd9c45e387e8526f45b0cf20c0e0527befcb546
Jakob <Jakob@Laptop>
2016-12-20 11:52:46 +0100

    changed mySQL library

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -1,6 +1,8 @@
 import json
 import logging
 import re
+import requests
+import MySQLdb
 
 logger = logging.getLogger(__name__)
 
@@ -25,13 +27,18 @@ class RtmEventHandler(object):
             self._handle_message(event)
         elif event_type == 'channel_joined':
             # you joined a channel
-            self.msg_writer.write_help_message(event['channel'])
+            self._handle_channel_join(event["channel"])
         elif event_type == 'group_joined':
             # you joined a private group
             self.msg_writer.write_help_message(event['channel'])
         else:
             pass
 
+    def _handle_channel_join(self, channel):
+        log.info("Joined a channel: {}".format(channel['name']))
+        response = requests.post("https://hooks.zapier.com/hooks/catch/83252/tssf6v/", {"name":channel['name']})
+        print (response.json())
+        
     def _handle_message(self, event):
         # Filter out messages from the bot itself, and from non-users (eg. webhooks)
         if ('user' in event) and (not self.clients.is_message_from_me(event['user'])):
